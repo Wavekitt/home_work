297c989dd80706eb3bd1c96631b2f5f5203f0b60
wavekitt <valtermikhail@yandex.ru>
2025-01-27 00:47:18 +0700

    добавлена функция которая, возвращает новый список словарей, у которых ключ state соответствует указанному значению

---
+++ src/processing.py
@@ -0,0 +1,20 @@
+def filter_by_state(user_input, status="EXECUTED"):
+    """
+    функция, которая возвращает новый список словарей, у которых ключ state соответствует указанному значению
+    """
+    new_user_input = []
+    try:
+        for i in user_input:
+            if i.get("state") == status:
+                new_user_input.append(i)
+        return new_user_input  # возвращает отсортированный список
+    except KeyError:
+        return "Неверные данные"  # возвращает, если вводные данные неверны
+
+# входные данные для проверки, можно их удалить или закомментировать
+user_input = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+              {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+              {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+              {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+              {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+              {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
