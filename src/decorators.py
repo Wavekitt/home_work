2cfffad9b8937f0423ae417964f2d35e566a7b45
wavekitt <valtermikhail@yandex.ru>
2025-02-15 16:14:12 +0700

    доабвлен декоратор для логов

---
+++ src/decorators.py
@@ -0,0 +1,39 @@
+from functools import wraps
+from typing import Any, Callable, Optional
+
+
+def logs(filename: Optional[str] = None) -> Callable:
+    """
+    Декоратор, нужный для логов в консоли
+    """
+
+    def logging_decorator(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            try:
+                result = func(*args, **kwargs)
+
+                logs_message = f"{func.__name__} is ok, Result: {result}"
+
+                if filename:
+                    with open(filename, "a") as file:
+                        file.write(logs_message + "\n")
+                else:
+                    print(logs_message)
+
+                return result
+
+            except Exception as error_:
+                logs_message = f"{func.__name__} error: {error_}, input: {args}, {kwargs}"
+
+                if filename:
+                    with open(filename, "a") as file:
+                        file.write(logs_message + "\n")
+                else:
+                    print(logs_message)
+
+                raise
+
+        return wrapper
+
+    return logging_decorator
