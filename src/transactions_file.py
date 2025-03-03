d883edb1d8d9f92f30127ca6e696eed30ed2f257
wavekitt <valtermikhail@yandex.ru>
2025-03-03 18:26:43 +0700

    исправил код, добавил докстринги

--- src/transactions_file.py
+++ src/transactions_file.py
@@ -1,26 +1,34 @@
-import csv
 from typing import Dict, List
-
 import pandas as pd
 
 
-def read_financial_operations_from_csv(filepath: str) -> List[Dict]:
-    transactions = []
-    with open(filepath, mode="r", encoding="utf-8") as file:
-        csv_reader = csv.DictReader(file)
-        for row in csv_reader:
-            transactions.append(row)
-    return transactions
+def read_financial_operations_from_csv(csv_file_path: str) -> List[Dict]:
+    """
+    функиця, которая считывает из csv файла и возвращает список словарей с транзакциями.
+    """
+    try:
+        csv_data = pd.read_csv(csv_file_path, delimiter=";")
+        operations_data = csv_data.to_dict(orient="records")
+    except pd.errors.EmptyDataError:
+        print(f"Файл {csv_file_path} не найден")
+        return []
+    except FileNotFoundError:
+        print(f"Файл {csv_file_path} не найден")
+        return []
+    return operations_data
 
 
 def read_financial_operations_from_excel(filepath: str) -> List[Dict]:
+    """
+    Функция принимает на вход путь к файлу Excel и возвращает список словарей с транзакциями.
+    """
     df = pd.read_excel(filepath)
     return df.to_dict(orient="records")
 
 
 if __name__ == "__main__":
-    csv_file_path = r"C:\Users\wavekitt\Desktop\transactions.csv"
-    excel_file_path = r"C:\Users\wavekitt\Desktop\transactions_excel.xlsx"
+    csv_file_path = r"C:\Users\wavekitt\PythonProject4\data\transactions.csv"
+    excel_file_path = r"C:\Users\wavekitt\PythonProject4\data\transactions_excel.xlsx"
 
     transactions_from_csv = read_financial_operations_from_csv(csv_file_path)
     transactions_from_excel = read_financial_operations_from_excel(excel_file_path)
